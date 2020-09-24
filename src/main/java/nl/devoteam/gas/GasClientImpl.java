2e8abbdcdb69716acea1c03625a08a555a43cb6c
Joran Bergfeld <joran.bergfeld@devoteam.com>
2020-09-24 17:31:03 +0200

    Implements ordering of payloads to messaging queues.

--- src/main/java/nl/devoteam/gas/GasClientImpl.java
+++ src/main/java/nl/devoteam/gas/GasClientImpl.java
@@ -1,5 +1,8 @@
 package nl.devoteam.gas;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.concurrent.CompletionStage;
 import java.util.logging.Logger;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
@@ -8,6 +11,7 @@ import javax.json.bind.JsonbBuilder;
 import org.eclipse.microprofile.reactive.messaging.Channel;
 import org.eclipse.microprofile.reactive.messaging.Emitter;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.jboss.logmanager.Level;
 
 @ApplicationScoped
 public class GasClientImpl implements GasClient {
@@ -25,10 +29,21 @@ public class GasClientImpl implements GasClient {
     @Override
     public void postGasData(GasData data) {
         Jsonb jsonb = JsonbBuilder.create();
-        final String payload = jsonb.toJson(data);
-        LOGGER.info("Sending payload to Pollution Channel: " + payload);
-        gasEmitter.send(payload);
-    }
+        Map<String, Object> map = new LinkedHashMap<>();
+        map.put("stationId", data.getStationId());
+        map.put("instant", data.getInstant());
+        map.put("adc", data.getAdc());
+        map.put("nh3", data.getNh3());
+        map.put("oxidising", data.getOxidising());
+        map.put("reducing", data.getReducing());
+        final String payload = jsonb.toJson(map);
+        LOGGER.info("Sending payload to Gas Channel: " + payload);
+        CompletionStage<Void> send = gasEmitter.send(payload);
+        send.whenComplete((unused, throwable) -> LOGGER.info("Managed to send gas data."))
+            .exceptionally(throwable -> {
+                LOGGER.log(Level.ERROR, "Failed to send message. Error: " + throwable.getMessage());
+                return null;
+            });    }
 
     @Override
     public GasData receiveGasData() {
--- src/main/java/nl/devoteam/pollution/PollutionClientImpl.java
+++ src/main/java/nl/devoteam/pollution/PollutionClientImpl.java
@@ -1,5 +1,7 @@
 package nl.devoteam.pollution;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.concurrent.CompletionStage;
 import java.util.logging.Logger;
 import javax.enterprise.context.ApplicationScoped;
@@ -28,12 +30,26 @@ public class PollutionClientImpl implements PollutionClient {
     @Override
     public void postPollutionData(PollutionData data) {
         Jsonb jsonb = JsonbBuilder.create();
-        final String payload = jsonb.toJson(data);
+        Map<String, Object> map = new LinkedHashMap<>();
+        map.put("stationId", data.getStationId());
+        map.put("instant", data.getInstant());
+        map.put("PM1_0", data.getPm1_0());
+        map.put("PM2_5", data.getPm2_5());
+        map.put("PM10", data.getPm10());
+        map.put("PM1_0_atm", data.getPm1_0_atm());
+        map.put("PM2_5_atm", data.getPm2_5_atm());
+        map.put("PM10_atm", data.getPm10_atm());
+        map.put("gt0_3um", data.getGt0_3um());
+        map.put("gt0_5um", data.getGt0_5um());
+        map.put("gt1_0um", data.getGt1_0um());
+        map.put("gt2_5um", data.getGt2_5um());
+        map.put("gt5_0um", data.getGt5_0um());
+        map.put("gt10um", data.getGt10um());
+        final String payload = jsonb.toJson(map);
+
         LOGGER.info("Sending payload to Pollution Channel: " + payload);
         CompletionStage<Void> send = pollutionEmitter.send(payload);
-        send.whenComplete((unused, throwable) -> {
-            LOGGER.info("Managed to send data.");
-        })
+        send.whenComplete((unused, throwable) -> LOGGER.info("Managed to send data."))
             .exceptionally(throwable -> {
                 LOGGER.log(Level.ERROR, "Failed to send message. Error: " + throwable.getMessage());
                 return null;
