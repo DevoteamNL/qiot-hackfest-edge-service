1d4857e9d781814268ed2377d597977aeae25f4a
Joran Bergfeld <joran.bergfeld@devoteam.com>
2020-09-28 20:09:47 +0200

    Removes cache from build. Removes tests.

--- src/test/java/nl/devoteam/NativeExampleResourceIT.java
+++
@@ -1,9 +0,0 @@
-package nl.devoteam;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeExampleResourceIT extends ExampleResourceTest {
-
-    // Execute the same tests but in native mode.
-}
